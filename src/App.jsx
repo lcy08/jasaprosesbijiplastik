dd914c636bdc29bcc7ed5c22a5e806227d07ea20
lcy08 <lucky.feliyanto@gmail.com>
2025-07-11 05:47:03 +0700

    starting

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import './index.css';
+
+function App() {
+  
+  return(
+    <>
+    <Header />
+    <Content />
+    <Footer />
+    </>
+  );
+}
+
+export default App
