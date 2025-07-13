368a425589362ad36089c35174b6caeddd5005a9
lcy08 <lucky.feliyanto@gmail.com>
2025-07-13 23:19:16 +0700

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default defineConfig([
       },
     },
     rules: {
+      "react/jsx-uses-vars": "error",
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
