579abdc519b688c197db303dc58b8414eb323b30
lcy08 <lucky.feliyanto@gmail.com>
2025-07-15 10:02:14 +0700

    optimization for font loading

--- src/App.jsx
+++ src/App.jsx
@@ -30,10 +30,11 @@ function App(){
     </motion.div>
 =======
 import Home from "./components/Home/Home"
-import About from "./components/About/About"
-import FAQ from "./components/FAQ/FAQ"
-import Pricing from "./components/Pricing/Pricing"
-import Contact from "./components/Contact/Contact"
+import React, { Suspense, lazy } from "react";
+const About = lazy(() => import("./components/About/About"));
+const FAQ = lazy(() => import("./components/FAQ/FAQ"));
+const Pricing = lazy(() => import("./components/Pricing/Pricing"));
+const Contact = lazy(() => import("./components/Contact/Contact"));
 
 function App(){
   const padding = {
@@ -42,13 +43,15 @@ function App(){
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Suspense fallback={<div className="text-center py-20">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Suspense>
     </Router>
 >>>>>>> dde4f7d (Router)
   )
