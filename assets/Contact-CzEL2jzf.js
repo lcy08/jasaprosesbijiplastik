d907d2b7b90073d96e4b46a9711d25d3bf348edf
lcy08 <lucky.feliyanto@gmail.com>
2025-07-15 10:36:45 +0700

    Updates

--- assets/Contact-CzEL2jzf.js
+++ assets/Contact-CzEL2jzf.js
@@ -1,4 +1,4 @@
-import{r as l,j as e,H as v,a as k,m as h,F as C}from"./index-BoetbXUn.js";/**
+import{r as l,j as e,H as v,a as k,m as h,F as C}from"./index-Dt6-DuyL.js";/**
  * @license lucide-react v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
