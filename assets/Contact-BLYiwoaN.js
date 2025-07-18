45bacf92e2af911fd4043ba0ce889ebba7eb293d
lcy08 <lucky.feliyanto@gmail.com>
2025-07-18 22:35:42 +0700

    Updates

--- assets/Contact-BLYiwoaN.js
+++ assets/Contact-BLYiwoaN.js
@@ -1,4 +1,4 @@
-import{c,r as d,j as e,H as x,a as h,B as p,m as a,F as u}from"./index-DLgYgSjs.js";/**
+import{c,r as d,j as e,H as x,a as h,B as p,m as a,F as u}from"./index-UJuSLYQW.js";/**
  * @license lucide-react v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
