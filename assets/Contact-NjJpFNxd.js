89426879bafc85c63912fcc95b425c950d1ec0b3
lcy08 <lucky.feliyanto@gmail.com>
2025-07-19 12:00:53 +0700

    Updates

--- assets/Contact-NjJpFNxd.js
+++ assets/Contact-NjJpFNxd.js
@@ -1,4 +1,4 @@
-import{c,r as d,j as e,H as x,a as h,B as p,m as a,F as u}from"./index-C9uULpEI.js";/**
+import{c,r as d,j as e,H as x,a as h,B as p,m as a,F as u}from"./index-DNUK0sTd.js";/**
  * @license lucide-react v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
