b68f27529ac2acd3d25c9cc5d9cc90d65a15232e
Lucky Feliyanto <lucky.feliyanto@gmail.com>
2026-06-26 17:19:41 +0700

    media 2

--- src/data/media.js
+++ src/data/media.js
@@ -1,9 +1,75 @@
 // src/data/media.js
 const media = [
-  { id: 1, type: 'image', src: '/Gallery/1.jpg', alt: 'Pic 1', category: 'Pencucian' },
-  { id: 2, type: 'video/mp4', src: '/Gallery/2.mp4', alt: 'Video 1', category: 'Pencucian' },
-  { id: 3, type: 'image', src: '/Gallery/3.jpg', alt: 'Pic 2', category: 'Penggilingan' },
-  { id: 4, type: 'video/mp4', src: '/Gallery/4.mp4', alt: 'Video 2', category: 'Penggilingan' },
+  {
+    id: 1,
+    type: "image",
+    src: "/Gallery/1.jpg",
+    alt: "Pic 1",
+    category: "Pencucian",
+  },
+  {
+    id: 2,
+    type: "video/mp4",
+    src: "/Gallery/2.mp4",
+    alt: "Video 1",
+    category: "Pencucian",
+  },
+  {
+    id: 3,
+    type: "image",
+    src: "/Gallery/3.jpg",
+    alt: "Pic 2",
+    category: "Penggilingan",
+  },
+  {
+    id: 4,
+    type: "video/mp4",
+    src: "/Gallery/4.mp4",
+    alt: "Video 2",
+    category: "Penggilingan",
+  },
+  {
+    id: 5,
+    type: "video/mp4",
+    src: "/Gallery/5.mp4",
+    alt: "Video 3",
+    category: "Hasil",
+  },
+  {
+    id: 6,
+    type: "video/mp4",
+    src: "/Gallery/6.mp4",
+    alt: "Video 4",
+    category: "Hasil",
+  },
+  {
+    id: 7,
+    type: "video/mp4",
+    src: "/Gallery/7.mp4",
+    alt: "Video 5",
+    category: "Hasil",
+  },
+  {
+    id: 8,
+    type: "image",
+    src: "/Gallery/8.jpeg",
+    alt: "Pic 3",
+    category: "Penyimpanan",
+  },
+  {
+    id: 9,
+    type: "video/mp4",
+    src: "/Gallery/9.mp4",
+    alt: "Video 6",
+    category: "Penyimpanan",
+  },
+  {
+    id: 10,
+    type: "video/mp4",
+    src: "/Gallery/10.mp4",
+    alt: "Video 7",
+    category: "Penyimpanan",
+  },
   // ... up to 50
 ];
 
