cf176bf39768f51abc18a82d5bc6cf6745d76a5d
Lucky Feliyanto <lucky.feliyanto@gmail.com>
2026-06-26 18:34:29 +0700

    media 3

--- src/data/media.js
+++ src/data/media.js
@@ -16,10 +16,14 @@ const media = [
     type: "video/mp4",
     src: "/Gallery/2.mp4",
 <<<<<<< HEAD
+<<<<<<< HEAD
     alt: "Video",
 =======
     alt: "Video 1",
 >>>>>>> a7ecc27 (media 2)
+=======
+    alt: "Video",
+>>>>>>> df43a26 (media 3)
     category: "Pencucian",
   },
   {
@@ -27,10 +31,14 @@ const media = [
     type: "image",
     src: "/Gallery/3.jpg",
 <<<<<<< HEAD
+<<<<<<< HEAD
     alt: "Pic",
 =======
     alt: "Pic 2",
 >>>>>>> a7ecc27 (media 2)
+=======
+    alt: "Pic",
+>>>>>>> df43a26 (media 3)
     category: "Penggilingan",
   },
   {
@@ -38,10 +46,14 @@ const media = [
     type: "video/mp4",
     src: "/Gallery/4.mp4",
 <<<<<<< HEAD
+<<<<<<< HEAD
     alt: "Video",
 =======
     alt: "Video 2",
 >>>>>>> a7ecc27 (media 2)
+=======
+    alt: "Video",
+>>>>>>> df43a26 (media 3)
     category: "Penggilingan",
   },
   {
@@ -49,10 +61,14 @@ const media = [
     type: "video/mp4",
     src: "/Gallery/5.mp4",
 <<<<<<< HEAD
+<<<<<<< HEAD
     alt: "Video",
 =======
     alt: "Video 3",
 >>>>>>> a7ecc27 (media 2)
+=======
+    alt: "Video",
+>>>>>>> df43a26 (media 3)
     category: "Hasil",
   },
   {
@@ -60,10 +76,14 @@ const media = [
     type: "video/mp4",
     src: "/Gallery/6.mp4",
 <<<<<<< HEAD
+<<<<<<< HEAD
     alt: "Video",
 =======
     alt: "Video 4",
 >>>>>>> a7ecc27 (media 2)
+=======
+    alt: "Video",
+>>>>>>> df43a26 (media 3)
     category: "Hasil",
   },
   {
@@ -71,10 +91,14 @@ const media = [
     type: "video/mp4",
     src: "/Gallery/7.mp4",
 <<<<<<< HEAD
+<<<<<<< HEAD
     alt: "Video",
 =======
     alt: "Video 5",
 >>>>>>> a7ecc27 (media 2)
+=======
+    alt: "Video",
+>>>>>>> df43a26 (media 3)
     category: "Hasil",
   },
   {
@@ -82,10 +106,14 @@ const media = [
     type: "image",
     src: "/Gallery/8.jpeg",
 <<<<<<< HEAD
+<<<<<<< HEAD
     alt: "Pic",
 =======
     alt: "Pic 3",
 >>>>>>> a7ecc27 (media 2)
+=======
+    alt: "Pic",
+>>>>>>> df43a26 (media 3)
     category: "Penyimpanan",
   },
   {
@@ -93,16 +121,21 @@ const media = [
     type: "video/mp4",
     src: "/Gallery/9.mp4",
 <<<<<<< HEAD
+<<<<<<< HEAD
     alt: "Video",
 =======
     alt: "Video 6",
 >>>>>>> a7ecc27 (media 2)
+=======
+    alt: "Video",
+>>>>>>> df43a26 (media 3)
     category: "Penyimpanan",
   },
   {
     id: 10,
     type: "video/mp4",
     src: "/Gallery/10.mp4",
+<<<<<<< HEAD
 <<<<<<< HEAD
     alt: "Video",
     category: "Penyimpanan",
@@ -230,6 +263,123 @@ const media = [
     category: "Penyimpanan",
   },
 >>>>>>> a7ecc27 (media 2)
+=======
+    alt: "Video",
+    category: "Penyimpanan",
+  },
+  {
+    id: 11,
+    type: "image",
+    src: "/Gallery/11.jpeg",
+    alt: "Pic",
+    category: "Penyimpanan",
+  },
+  {
+    id: 12,
+    type: "image",
+    src: "/Gallery/12.jpeg",
+    alt: "Pic",
+    category: "Penyimpanan",
+  },
+  {
+    id: 13,
+    type: "video/mp4",
+    src: "/Gallery/13.mp4",
+    alt: "Video",
+    category: "Hasil",
+  },
+  {
+    id: 14,
+    type: "image",
+    src: "/Gallery/14.mp4",
+    alt: "Pic",
+    category: "Hasil",
+  },
+  {
+    id: 15,
+    type: "video/mp4",
+    src: "/Gallery/15.mp4",
+    alt: "Video",
+    category: "Hasil",
+  },
+  {
+    id: 16,
+    type: "image",
+    src: "/Gallery/16.jpeg",
+    alt: "Pic",
+    category: "Hasil",
+  },
+  {
+    id: 17,
+    type: "video/mp4",
+    src: "/Gallery/17.mp4",
+    alt: "Video",
+    category: "Hasil",
+  },
+  {
+    id: 18,
+    type: "image",
+    src: "/Gallery/18.jpeg",
+    alt: "Pic",
+    category: "Hasil",
+  },
+  {
+    id: 19,
+    type: "video/mp4",
+    src: "/Gallery/19.mp4",
+    alt: "Video",
+    category: "Hasil",
+  },
+  {
+    id: 20,
+    type: "image",
+    src: "/Gallery/20.jpeg",
+    alt: "Pic",
+    category: "Hasil",
+  },
+  {
+    id: 21,
+    type: "video/mp4",
+    src: "/Gallery/21.mp4",
+    alt: "Video",
+    category: "Hasil",
+  },
+  {
+    id: 22,
+    type: "image",
+    src: "/Gallery/22.jpeg",
+    alt: "Pic",
+    category: "Hasil",
+  },
+  {
+    id: 23,
+    type: "video/mp4",
+    src: "/Gallery/23.mp4",
+    alt: "Video",
+    category: "Hasil",
+  },
+  {
+    id: 24,
+    type: "video/mp4",
+    src: "/Gallery/24.mp4",
+    alt: "Video",
+    category: "Hasil",
+  },
+  {
+    id: 25,
+    type: "video/mp4",
+    src: "/Gallery/25.mp4",
+    alt: "Video",
+    category: "Hasil",
+  },
+  {
+    id: 26,
+    type: "image",
+    src: "/Gallery/26.jpeg",
+    alt: "Pic",
+    category: "Hasil",
+  },
+>>>>>>> df43a26 (media 3)
   // ... up to 50
 ];
 
